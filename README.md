52dffbefd21f039c3a42be07d7a9735f96178af9
shahriarahmmed <shahariarahmmed.sa@gmail.com>
2026-02-16 17:00:50 +0600

    modal function add and readme file add for question

---
+++ README.md
@@ -0,0 +1,5 @@
+## 1. What is the difference between null and undefined?
+## 2. What is the use of the map() function in JavaScript? How is it different from forEach()?
+## 3. What is the difference between == and ===?
+## 4. What is the significance of async/await in fetching API data?
+## 5. Explain the concept of Scope in JavaScript (Global, Function, Block).
--- script/app.js
+++ script/app.js
@@ -8,7 +8,7 @@ const displayProducts = (products) => {
     const productsContainer = document.getElementById("products-container");
     productsContainer.innerHTML = "";
     products.forEach((product) => {
-        console.log(product);
+        // console.log(product);
         const productCard = document.createElement("div");
         productCard.innerHTML = `
            <div class="card w-full bg-base-100 shadow-sm">
@@ -28,15 +28,51 @@ const displayProducts = (products) => {
                         <h2 class="card-title">${product.title}</h2>
                         <p class="text-xl font-bold">$${product.price}</p>
                         <div class="card-actions justify-center ">
-                            <button class="btn btn-outline px-10">Details</button>
+                            <button class="btn" onclick="displayProductsDetails(${product.id})" btn-outline px-10">Details</button>
                             <button class="btn btn-primary px-10">Add</button>
 
                         </div>
                     </div>
                 </div>
         `;
+        productCard.querySelector("button").addEventListener("click", () => {
+            my_modal_5.showModal();
+        });
         productsContainer.append(productCard);
     });
+}
+
+const loadProductDetails = (id) => {
+    const url = `https://fakestoreapi.com/products/${id}`;
+    // console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(json => displayProductsDetails(json));
+}
+
+const displayProductsDetails = (product) => {
+    console.log(product);
+    const productDetailsContainer = document.getElementById("product-details");
+    productDetailsContainer.innerHTML = `
+                    <div class="card w-full bg-base-100 shadow-sm">
+                        <div class="card-body">
+                            
+
+                            <h2 class="card-title">${product.id} title not showing</h2>
+                            <p class="text-xl font-bold">$${product.description}</p>
+                            <p class="text-xl font-bold">$${product.price}</p>
+                            
+                            <button class="btn btn-primary px-10">Add to cart</button>
+                        </div>
+                    </div>
+                <div class="modal-action">
+                        <form method="dialog">
+                            <!-- if there is a button in form, it will close the modal -->
+                            <button class="btn">Close</button>
+                        </form>
+                    </div>
+    `;
+    document.getElementById("my_modal_5").showModal();
 }
 
 loadProducts();
